9dfe0d70f601dbbb4282c984d96695d1185aeebb
Newton Duarte Cavalcanti <newton_@hotmail.com>
2021-07-05 05:26:04 -0300

    fix: check if player exists to move camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,26 +17,29 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        // Camera's current position
-        Vector3 startPos = transform.position;
+        if (player)
+        {
+            // Camera's current position
+            Vector3 startPos = transform.position;
 
-        // Player's current position
-        Vector3 endPos = player.transform.position;
+            // Player's current position
+            Vector3 endPos = player.transform.position;
 
-        endPos.x += posOffset.x;
-        endPos.y += posOffset.y;
-        endPos.z = -10;
+            endPos.x += posOffset.x;
+            endPos.y += posOffset.y;
+            endPos.z = -10;
 
-        // Smoothly move the camera towards the player's posistion
-        transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+            // Smoothly move the camera towards the player's posistion
+            transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
 
-        transform.position = new Vector3
-        (
-            // Mathf.Clamp is a way to limit a value to a particular range
-            // Here we're limiting x position of the camera between leftLimit and rightLimit
-            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
-            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
-            transform.position.z
-        );
+            transform.position = new Vector3
+            (
+                // Mathf.Clamp is a way to limit a value to a particular range
+                // Here we're limiting x position of the camera between leftLimit and rightLimit
+                Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+                Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+                transform.position.z
+            );
+        }
     }
 }
